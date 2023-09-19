706e186e7cc4f3c79340ec73e7eea515b195836d
Doltons Edward Nicholas <edwarddoltons@gmail.com>
2023-09-19 21:03:28 +0800

    init landing page

---
+++ script.js
@@ -0,0 +1,48 @@
+// let, var
+const nama = "doltons";
+const umurDariNama = 20; // number
+const boxBuah = ["mangga", "anggur", "apel", "jeruk", "pisang"]; // array
+const sepuh = {
+  nama: "Ilham",
+  umur: 20,
+  isMarried: false,
+  favFood: "bakso",
+};
+
+// init, condition, action
+// for (let i = 0; i < 4; i++) {
+//   console.log(boxBuah[i]);
+// }
+
+// for in
+for (const buah of boxBuah) {
+  console.log(buah);
+}
+
+// for of
+// for (const item in sepuh) {
+//   console.log(sepuh[item]);
+// }
+
+// // loop 1
+// const item = "nama";
+// sepuh[item];
+// // loop 2
+
+// const item = "umur";
+// sepuh[item];
+
+// console.log(sepuh.nama);
+// console.log(sepuh.umur);
+// console.log(sepuh.isMarried);
+// console.log(sepuh.favFood);
+
+// console.log(boxBuah[0]);
+// console.log(boxBuah[1]);
+// console.log(boxBuah[2]);
+// console.log(boxBuah[3]);
+
+// loop
+// for loop
+// while loop
+// for in / for of
