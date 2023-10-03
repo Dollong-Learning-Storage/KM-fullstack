f9ec1216008b3fb8d2b0fce018677dbda8ea3dd8
Doltons Edward Nicholas <edwarddoltons@gmail.com>
2023-10-03 21:25:31 +0800

    feat: create digiapp

---
+++ Digiapp/data.js
@@ -0,0 +1,33 @@
+const DIGIMON_DATA = [
+  {
+    id: 1,
+    name: "Garummon",
+    href: "https://www.digi-api.com/api/v1/digimon/1",
+    image: "https://www.digi-api.com/images/digimon/w/Garummon.png",
+  },
+  {
+    id: 2,
+    name: "Craniummon",
+    href: "https://www.digi-api.com/api/v1/digimon/2",
+    image: "https://www.digi-api.com/images/digimon/w/Craniummon.png",
+  },
+  {
+    id: 3,
+    name: "Holy Angemon",
+    href: "https://www.digi-api.com/api/v1/digimon/3",
+    image: "https://www.digi-api.com/images/digimon/w/Holy_Angemon.png",
+  },
+  {
+    id: 4,
+    name: "Ancient Beatmon",
+    href: "https://www.digi-api.com/api/v1/digimon/4",
+    image: "https://www.digi-api.com/images/digimon/w/Ancient_Beatmon.png",
+  },
+  {
+    id: 5,
+    name: "Dukemon(Crimson Mode)",
+    href: "https://www.digi-api.com/api/v1/digimon/5",
+    image:
+      "https://www.digi-api.com/images/digimon/w/Dukemon(Crimson_Mode).png",
+  },
+];
---
+++ Digiapp/script.js
@@ -0,0 +1,7 @@
+const clickBtn = document.getElementById("click-btn");
+
+function panggilAlert() {
+  alert("test");
+}
+
+clickBtn.onclick = panggilAlert;
